e264fa6895dda20e21347d5b5642496f82b71ad4
Fathi Auni <94261994+FathiAuni05@users.noreply.github.com>
2024-12-10 15:31:04 +0800

    Update Beyonce.py

--- pages/Beyonce.py
+++ pages/Beyonce.py
@@ -43,7 +43,7 @@ nltk.download('words')
 english_words = set(words.words())
 
 # Preprocess Data
-tokens = word_tokenize(" ".join(df['lyric']))
+tokens = word_tokenize(" ".join(df['Lyric']))
 english_stopwords = set(stopwords.words('english'))
 
 # Filter out stopwords, punctuation, and words starting with apostrophe
