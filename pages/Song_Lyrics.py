114944f702cf3e53db9900562b5730e275e0a9c1
Fathi Auni <94261994+FathiAuni05@users.noreply.github.com>
2025-01-04 22:33:22 +0800

    Update Song_Lyrics.py

--- pages/Song_Lyrics.py
+++ pages/Song_Lyrics.py
@@ -49,7 +49,7 @@ nltk.download('punkt_tab') # Download the Punkt Tokenizer Models
 nltk.download('words')
 
 # Preprocess Data
-tokens = word_tokenize(" ".join(df['lyric']))
+tokens = word_tokenize(" ".join(df['lyrics']))
 english_stopwords = set(stopwords.words('english'))
 
 # Filter out stopwords, punctuation, and words starting with apostrophe
