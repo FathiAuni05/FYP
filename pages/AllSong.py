01925e579369d8fc4b6b223fe5f1f2ba29846a68
Fathi Auni <94261994+FathiAuni05@users.noreply.github.com>
2025-01-22 01:49:25 +0800

    Update AllSong.py

--- pages/AllSong.py
+++ pages/AllSong.py
@@ -41,13 +41,11 @@ st.subheader("The words")
 # Download the necessary NLTK data packages
 nltk.download('punkt_tab') # Download the Punkt Tokenizer Models
 nltk.download('words')
-nltk.download('punkt')
 
 english_words = set(words.words())
 
 # Preprocess Data
 tokens = word_tokenize(" ".join(df['lyric']))
-st.write(tokens)
 english_stopwords = set(stopwords.words('english'))
 
 # Filter out stopwords, punctuation, and words starting with apostrophe
